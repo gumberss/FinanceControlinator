ffe73f35bedadecb8de768856ed0aaecdd39a3ac
gumberss <gustavomarinsuppi@gmail.com>
2021-09-19 20:50:00 -0300

    chore: remove unused using

--- Microservices/PiggyBanks/PiggyBanks.Application/AppServices/PiggyBankAppService.cs
+++ Microservices/PiggyBanks/PiggyBanks.Application/AppServices/PiggyBankAppService.cs
@@ -9,7 +9,6 @@ using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net;
 using System.Threading.Tasks;
 using PiggyBanks.Domain.Localizations;
 
