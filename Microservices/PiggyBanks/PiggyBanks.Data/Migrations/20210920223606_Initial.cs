f45bf726f40c598effbeaf8d7ad02168b902335b
gumberss <gustavomarinsuppi@gmail.com>
2021-09-20 19:48:35 -0300

    chore: adding initial db context design

---
+++ Microservices/PiggyBanks/PiggyBanks.Data/Migrations/20210920223606_Initial.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PiggyBanks.Data.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "piggyBanks");
+
+            migrationBuilder.CreateTable(
+                name: "PiggyBanks",
+                schema: "piggyBanks",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Type = table.Column<int>(type: "int", nullable: false),
+                    GoalDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    GoalValue = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    SavedValue = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    StartDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdatedDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    Version = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PiggyBanks", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PiggyBanks",
+                schema: "piggyBanks");
+        }
+    }
+}
