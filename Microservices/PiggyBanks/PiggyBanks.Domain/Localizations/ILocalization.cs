2a798a7a7e8aecc04244bd8ed57e4d74021b31fe
gumberss <gustavomarinsuppi@gmail.com>
2021-09-22 22:31:28 -0300

    feat: adding piggy bank validation rules

--- Microservices/PiggyBanks/PiggyBanks.Domain/Localizations/ILocalization.cs
+++ Microservices/PiggyBanks/PiggyBanks.Domain/Localizations/ILocalization.cs
@@ -5,5 +5,11 @@ namespace PiggyBanks.Domain.Localizations
     public interface ILocalization
     {
         String PIGGY_BANK_ALREADY_EXISTS_BY_TITLE { get; }
+        string PIGGY_BANK_INVALID_GOAL_VALUE { get; }
+        string PIGGY_BANK_INVALID_SAVED_VALUE { get; }
+        string PIGGY_BANK_INVALID_START_DATE { get; }
+        string PIGGY_BANK_WITHOUT_TITLE { get; }
+        string PIGGY_BANK_INVALID_GOAL_DATE { get; }
+        string PIGGY_BANK_WITH_BIG_DESCRIPTION { get; }
     }
 }
--- Microservices/PiggyBanks/PiggyBanks.Domain/Localizations/Ptbr.cs
+++ Microservices/PiggyBanks/PiggyBanks.Domain/Localizations/Ptbr.cs
@@ -4,5 +4,17 @@ namespace PiggyBanks.Domain.Localizations
     public class Ptbr : ILocalization
     {
         public string PIGGY_BANK_ALREADY_EXISTS_BY_TITLE => "Já existe um cofrinho registrado com esse título";
+
+        public string PIGGY_BANK_INVALID_GOAL_VALUE => "O valor objetivo do cofrinho está inválido";
+
+        public string PIGGY_BANK_INVALID_SAVED_VALUE => "O valor já guardado para o cofrinho está inválido";
+
+        public string PIGGY_BANK_INVALID_START_DATE => "A data inicial do cofrinho está inválida";
+
+        public string PIGGY_BANK_WITHOUT_TITLE => "O cofrinho precisa ter um título";
+
+        public string PIGGY_BANK_INVALID_GOAL_DATE => "A data de objetivo do cofrinho está inválida";
+
+        public string PIGGY_BANK_WITH_BIG_DESCRIPTION => "A descrição do cofrinho está muito grande";
     }
 }
