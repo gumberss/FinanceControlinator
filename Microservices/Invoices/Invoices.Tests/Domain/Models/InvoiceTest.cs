f064d9f17aa8be3e08adf33a4dfe7818d2769482
gumberss <gustavomarinsuppi@gmail.com>
2021-10-01 12:00:34 -0300

    test: adding test for invoices add new itemmethod

--- Microservices/Invoices/Invoices.Tests/Domain/Models/InvoiceTest.cs
+++ Microservices/Invoices/Invoices.Tests/Domain/Models/InvoiceTest.cs
@@ -30,5 +30,20 @@ namespace Invoices.Tests.Domain.Models
             invoice.PaymentStatus.Should().Be(PaymentStatus.Paid);
             invoice.UpdatedDate.Should().BeOnOrAfter(datePaid);
         }
+
+        [TestMethod]
+        [JourneyCategory(TestUserJourneyEnum.RecordingExpenses)]
+        [IntegrationTestCategory(TestMicroserviceEnum.Invoices, TestFeatureEnum.InvoiceGeneration)]
+        public void Should_add_an_item_to_invoce()
+        {
+            var invoice = new Invoice(DateTime.Now);
+            var item = new InvoiceItem(10, 100);
+            
+            invoice.AddNew(item);
+
+            invoice.Items.Should().HaveCount(1);
+            invoice.Items.First().Should().Be(item);
+        }
+
     }
 }
