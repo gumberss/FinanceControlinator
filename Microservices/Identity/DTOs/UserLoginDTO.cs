878951557b744f73aa4c674294ebe0bfd0a996fb
gumberss <gustavomarinsuppi@gmail.com>
2022-04-21 18:21:44 -0300

    feat: adding identity microservice Details: 1. Creation of identity microservice as Minimum API 2. Adding Token Service that create the JWT token based on parameters 3. Adding JWT parameters in the app settings 4. Adding AspNetCore.Identity 5. Adding Identity connection string in the app settings 6. Adding identity Extension 7. Adding MigrationsExtension (to migrate automatically when the api start 8. Adding inital migrations with the command Add-Migration Identity-Initial 9. Authenticate endpoint creation to generate the token 10. SignUp endpoint creation to add a new user

---
+++ Microservices/Identity/DTOs/UserLoginDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace Identity.DTOs
+{
+    public record UserLoginDTO
+    {
+        public String UserName { get; set; }
+
+        public String Password { get; set; }
+
+        public String Email { get; set; }
+    }
+}
---
+++ Microservices/Identity/Extensions/IdentityExtension.cs
@@ -0,0 +1,19 @@
+﻿using Identity.Contexts;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace Identity.Extensions
+{
+    public static class IdentityExtension
+    {
+        public static void AddIdentity(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+                        .AddEntityFrameworkStores<IdentityAppDbContext>();
+
+            builder.Services.AddDbContext<IdentityAppDbContext>(
+                         options => options
+                         .UseSqlServer(builder.Configuration.GetConnectionString("DbConnection")));
+        }
+    }
+}
