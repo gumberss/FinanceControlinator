459239218bd173600750b37de5fc9710b85264aa
gumberss <gustavomarinsuppi@gmail.com>
2022-05-07 10:10:42 -0300

    chore: adding expense DTOs to make DDD template a little better. And its adapter (autoMapper)

---
+++ Microservices/Expenses/Expenses.DTO/Expenses/ExpenseItemDTO.cs
@@ -0,0 +1,15 @@
+﻿namespace Expenses.DTO.Expenses
+{
+    public record ExpenseItemDTO
+    {
+        public String Name { get; set; }
+
+        public String Description { get; set; }
+
+        public decimal Cost { get; set; }
+
+        public int Amount { get; set; }
+
+        public Guid ExpenseId { get; set; }
+    }
+}
