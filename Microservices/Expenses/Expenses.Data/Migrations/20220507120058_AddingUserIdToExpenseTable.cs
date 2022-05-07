16827f89ba5c272f3d81e7b9baa5f13520e1f8d8
gumberss <gustavomarinsuppi@gmail.com>
2022-05-07 10:14:57 -0300

    feat: adding expense UserId configuration and its index

---
+++ Microservices/Expenses/Expenses.Data/Migrations/20220507120058_AddingUserIdToExpenseTable.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Expenses.Data.Migrations
+{
+    public partial class AddingUserIdToExpenseTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "UserId",
+                schema: "expenses",
+                table: "Expenses",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                schema: "expenses",
+                table: "Expenses");
+        }
+    }
+}
