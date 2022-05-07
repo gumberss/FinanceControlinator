820216152b662b06cc851cada7efc0459846c867
gumberss <gustavomarinsuppi@gmail.com>
2022-05-07 10:10:42 -0300

    chore: adding expense DTOs to make DDD template a little better. And its adapter (autoMapper)

---
+++ Microservices/Expenses/Expenses.DTO/Expenses/ExpenseDTO.cs
@@ -0,0 +1,26 @@
+﻿namespace Expenses.DTO.Expenses
+{
+    public record ExpenseDTO
+    {
+        public String Title { get; set; }
+
+        public String Description { get; set; }
+
+        public DateTime PurchaseDate { get; set; }
+
+        //TODO: use only as ExpenseType, but it is not possible while the enum ExpenseType from models are using as parameter in API
+        public ExpenseTypeDTO Type { get; set; }
+
+        public int InstallmentsCount { get; set; }
+
+        public String Location { get; set; }
+
+        public String Observation { get; set; }
+
+        public decimal TotalCost { get; set; }
+
+        public Guid UserId { get; set; }
+
+        public List<ExpenseItemDTO> Items { get; set; }
+    }
+}
---
+++ Microservices/Expenses/Expenses.DTO/Expenses/ExpenseTypeDTO.cs
@@ -0,0 +1,17 @@
+﻿namespace Expenses.DTO.Expenses
+{
+    public enum ExpenseTypeDTO
+    {
+        Market = 0,
+
+        Bill = 10,
+
+        Leisure = 20,
+
+        Investment = 30,
+
+        Health = 40,
+
+        Other = 900
+    }
+}
