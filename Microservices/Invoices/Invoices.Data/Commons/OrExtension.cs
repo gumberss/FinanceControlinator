6e81dcd4bda55beec75744ad5ad5d46d959a3244
gumberss <gustavomarinsuppi@gmail.com>
2022-05-13 08:33:53 -0300

    chore: creating a class that combine two functions as OR. If you want to use and, you just use two wheres and that's it. But for Or it is not possible without this class

--- Microservices/Invoices/Invoices.Data/Commons/OrExtension.cs
+++ Microservices/Invoices/Invoices.Data/Commons/OrExtension.cs
@@ -3,21 +3,23 @@ using System.Linq.Expressions;
 
 namespace Invoices.Data.Commons
 {
-    public class Or
+    public static class OrExtension
     {
-        public static Expression<Func<T, bool>> Combine<T>(Func<T, bool> left, Func<T, bool> right)
+        public static Expression<Func<T, bool>> Or<T>(this Func<T, bool> left, Func<T, bool> right)
         {
             if (left == null && right == null) throw new ArgumentException("At least one argument must not be null");
             if (left == null) return Expression.Lambda<Func<T, bool>>(Expression.Call(left.Method));
             if (right == null) return Expression.Lambda<Func<T, bool>>(Expression.Call(right.Method));
 
-            var leftExpression = Expression.Lambda<Func<T, bool>>(Expression.Call(left.Method));
-            var rightExpression = Expression.Lambda<Func<T, bool>>(Expression.Call(right.Method));
+            var leftExpression = ToExpression(left);
+            var rightExpression = ToExpression(right);
 
             var parameter = Expression.Parameter(typeof(T), "p");
             var combined = new ParameterReplacer(parameter).Visit(Expression.OrElse(leftExpression.Body, rightExpression.Body));
             return Expression.Lambda<Func<T, bool>>(combined, parameter);
         }
+
+        private static Expression<Func<T, bool>> ToExpression<T>(Func<T, bool> left) => x => left(x);
 
         class ParameterReplacer : ExpressionVisitor
         {
