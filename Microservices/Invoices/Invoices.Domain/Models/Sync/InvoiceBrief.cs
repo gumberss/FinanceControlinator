18984d6455c202a3970e82c8358ef962b5d4858e
gumberss <gustavomarinsuppi@gmail.com>
2022-05-13 08:31:33 -0300

    feat: adding status to invoice overview and invoice brief

--- Microservices/Invoices/Invoices.Domain/Models/Sync/InvoiceBrief.cs
+++ Microservices/Invoices/Invoices.Domain/Models/Sync/InvoiceBrief.cs
@@ -4,11 +4,20 @@ namespace Invoices.Domain.Models.Sync
 {
     public record InvoiceBrief
     {
-        public InvoiceBrief(String text)
+        public InvoiceBrief(String text, InvoiceBriefStatus status = InvoiceBriefStatus.Default)
         {
             Text = text;
+            Status = status;
         }
 
         public String Text { get; set; }
+        public InvoiceBriefStatus Status { get; set; }
+    }
+
+    public enum InvoiceBriefStatus
+    {
+        Default = 0,
+        Danger = 1,
+        Safe = 2,
     }
 }
--- Microservices/Invoices/Invoices.Domain/Models/Sync/InvoiceOverview.cs
+++ Microservices/Invoices/Invoices.Domain/Models/Sync/InvoiceOverview.cs
@@ -6,7 +6,9 @@ namespace Invoices.Domain.Models.Sync
     {
         public string Date { get; set; }
 
-        public string Status { get; set; }
+        public string StatusText { get; set; }
+
+        public InvoiceOverviewStatus Status { get; set; }
 
         public string TotalCost { get; set; }
 
@@ -14,10 +16,28 @@ namespace Invoices.Domain.Models.Sync
 
         public List<InvoicePartition> Partitions { get; set; }
 
-        public InvoiceOverview(List<InvoiceBrief> briefs, List<InvoicePartition> partitions)
+        public InvoiceOverview(string date, 
+            string statusText, 
+            InvoiceOverviewStatus status, 
+            string totalCost, 
+            List<InvoiceBrief> briefs, 
+            List<InvoicePartition> partitions)
         {
+            Date = date;
+            StatusText = statusText;
+            Status = status;
+            TotalCost = totalCost;
             Briefs = briefs;
             Partitions = partitions;
         }
+    }
+
+    public enum InvoiceOverviewStatus
+    {
+        Overdue = 0,
+        Paid = 1,
+        Closed = 2,
+        Next = 3,
+        Open = 4
     }
 }
