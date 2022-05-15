abeec121d67a70986d67064720867ab8f5e65436
gumberss <gustavomarinsuppi@gmail.com>
2022-05-15 20:47:38 -0300

    chore: adding invoice status enum

---
+++ Microservices/Invoices/Invoices.Domain/Enums/InvoiceStatus.cs
@@ -0,0 +1,11 @@
+﻿namespace Invoices.Domain.Enums
+{
+    public enum InvoiceStatus
+    {
+        Overdue = 0,
+        Paid = 1,
+        Closed = 2,
+        Future = 3,
+        Open = 4
+    }
+}
