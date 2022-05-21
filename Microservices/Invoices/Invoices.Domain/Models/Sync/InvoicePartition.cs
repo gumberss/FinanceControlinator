a4a8596c0303b7723e342aab239918ec6025367f
gumberss <gustavomarinsuppi@gmail.com>
2022-05-21 09:43:19 -0300

    feat: adding TypeText to return type text converted based on localization

--- Microservices/Invoices/Invoices.Domain/Models/Sync/InvoicePartition.cs
+++ Microservices/Invoices/Invoices.Domain/Models/Sync/InvoicePartition.cs
@@ -4,17 +4,17 @@ namespace Invoices.Domain.Models.Sync
 {
     public record InvoicePartition
     {
-        public InvoicePartition(InvoiceItemType type, float percent, decimal totalValue)
+        public InvoicePartition(InvoiceItemType type, string typeText, decimal percent, decimal totalValue)
         {
             Type = type;
             Percent = percent;
+            TypeText = typeText;
             TotalValue = totalValue;
         }
 
         public InvoiceItemType Type { get; set; }
-
-        public float Percent { get; set; }
-
+        public decimal Percent { get; set; }
+        public string TypeText { get; set; }
         public decimal TotalValue { get; set; }
     }
 }
